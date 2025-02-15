c76598af36b19b4d3f8bcd180c3f3baa88eca957
anubhav0908 <singhanubhav8100@gmail.com>
2025-02-15 10:37:39 +0530

    [Anubhav]Add. Auto Closing Resource

---
+++ Day-04/src/main/java/exceptions/autoclosingresource/ReadFile.java
@@ -0,0 +1,13 @@
+package exceptions.autoclosingresource;
+import java.io.*;
+public class ReadFile {
+    public static void main(String[] args) {
+        String fileName = "info.txt";
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String firstLine = reader.readLine();
+            System.out.println(firstLine);
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+        }
+    }
+}
